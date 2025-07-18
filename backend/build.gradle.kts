fab9e81760466af135c4646004d4f8a06c13529b
Dominik Tomczak <dominiktomczak@gmail.com>
2025-07-18 12:22:11 +0200

    Wersje SDK

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "2.2.0"
+	kotlin("plugin.spring") version "2.2.0"
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -8,11 +8,11 @@ plugins {
 group = "com.opteduai"
 version = "0.0.1-SNAPSHOT"
 
-java {
+/*java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(17)
 	}
-}
+}*/
 
 configurations {
 	compileOnly {
